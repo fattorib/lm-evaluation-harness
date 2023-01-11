d92635507eaa6d7a42284ee85632ac7839dc229f
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2023-01-10 23:34:55 -0500

    update custom_tokenizer to use byt5

--- lm_eval/models/custom_tokenizer.py
+++ lm_eval/models/custom_tokenizer.py
@@ -1,5 +1,6 @@
 import torch 
 from typing import List 
+from transformers import AutoTokenizer
 
 class ByteTokenizer:
 
@@ -7,10 +8,26 @@ class ByteTokenizer:
         self.eos_token_id = 256 
         self.vocab_size = 257
 
+        self.sub_tokenizer = AutoTokenizer.from_pretrained("google/byt5-small")
+
     def encode(self,text: str) -> bytes:
         # encode a string of text as a bytearray 
-        return torch.tensor([list(text.encode("utf-8"))])
+        # call byt5 tokenizer then shift outputs 
+
+        encoded = torch.tensor(self.sub_tokenizer(text).input_ids) - 3
+        return {'input_ids': encoded[:-1]}
 
     def decode(self, tokens: List[int]) -> str:
         # decode a list of bytes to string 
         return bytearray(tokens[0]).decode('utf-8')
+
+
+if __name__ == '__main__':
+
+    tokenizer = AutoTokenizer.from_pretrained("google/byt5-small")
+
+    test = 'hey this is a test sentence!'
+
+    print(tokenizer(test))
+
+    print(ByteTokenizer().encode(test))
